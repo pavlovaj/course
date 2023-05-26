a21f23f1a583ebd4612eca63b557be5791f12ed4
pavlovaj <124414216+pavlovaj@users.noreply.github.com>
2023-05-27 02:58:54 +0700

    Update task2_1.py

--- task 2/task2_1.py
+++ task 2/task2_1.py
@@ -1,11 +1,11 @@
 import csv
 
-with open('images.csv', 'w') as a:
-    with open('stage3_test.csv', 'r') as b:
+with open('new_table.csv', 'w', encoding='UTF-8') as a:
+    fieldnames = ["Id", "Images", "Title", "Description", "Price"]
+    writer = csv.DictWriter(a, fieldnames=fieldnames)
+    writer.writeheader()
+    with open('stage3_test.csv', 'r', encoding='UTF-8') as b:
         reader = csv.DictReader(b, delimiter=',')
-        headers = reader.fieldnames
         for row in reader:
-            if len(row['Images'].split(',')) > 3:
-                print(row['Images'])
-                writer = csv.DictWriter(a, fieldnames=headers)
+            if len(row["Images"].split(',')) > 3:
                 writer.writerow(row)
