7ad37ad5dad8ba036af43169f5fb8ab2a7a562b5
pavlovaj <124414216+pavlovaj@users.noreply.github.com>
2023-05-27 03:01:13 +0700

    Update task2_3.py

--- task 2/task2_3.py
+++ task 2/task2_3.py
@@ -1,9 +1,9 @@
 import csv
 
-with open("stage3_test.csv", 'r') as f:
-    reader = csv.reader(f, delimiter=',')
+with open("stage3_test.csv", 'r') as a:
+    reader = csv.reader(a, delimiter=',')
 
-    with open('itp.csv', 'w') as f:
-        writer = csv.writer(f)
+    with open('itp.csv', 'w') as b:
+        writer = csv.writer(b)
         for row in reader:
             writer.writerow([row[0], row[2], row[4]])
