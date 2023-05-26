aa0d93c339554223fa2a36337305421aa2b18226
pavlovaj <pvlvaj@mail.ru>
2023-05-27 03:06:55 +0700

    задания семинара 2

--- task 2/task2_1.py
+++ task 2/task2_1.py
@@ -1,11 +1,10 @@
 import csv
 
-with open('images.csv', 'w') as a:
-    with open('stage3_test.csv', 'r') as b:
-        reader = csv.DictReader(b, delimiter=',')
+with open('images.csv', 'w') as f1:
+    with open('stage3_test.csv', 'r') as f2:
+        reader = csv.DictReader(f1, delimiter=',')
         headers = reader.fieldnames
         for row in reader:
             if len(row['Images'].split(',')) > 3:
-                print(row['Images'])
-                writer = csv.DictWriter(a, fieldnames=headers)
                 writer.writerow(row)
+
