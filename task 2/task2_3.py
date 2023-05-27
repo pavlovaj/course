334e1d0792e4ec1bdb3c46d02eb1422b8ab54670
pavlovaj <124414216+pavlovaj@users.noreply.github.com>
2023-05-27 10:36:24 +0700

    Delete task 2 directory

--- task 2/task2_3.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-with open("stage3_test.csv", 'r') as a:
-    reader = csv.reader(a, delimiter=',')
-
-    with open('itp.csv', 'w') as b:
-        writer = csv.writer(b)
-        for row in reader:
-            writer.writerow([row[0], row[2], row[4]])
